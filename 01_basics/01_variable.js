8686ba43a6fbb2f0406808aab4c3f8d953ccd9fd
niteshkumar956 <113203650+niteshkumar956@users.noreply.github.com>
2025-01-21 20:15:12 +0000

    learning

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 1200340
+let accountEmail = "nitesh@google.com"
+var accountpassword = "12345"
+accountCity = "jaipur"
+let accountState;
+//accountId = 2
+
+accountEmail = "nds@d"
+accountpassword = "21212121"
+accountCity = "Noida"
+console.log(accountId);
+
+console.table([accountId, accountEmail , accountpassword, accountCity,accountState])
