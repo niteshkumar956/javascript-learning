dd82910da7e041fd6d9a0129cf3bc9069291193c
niteshkumar956 <113203650+niteshkumar956@users.noreply.github.com>
2025-01-21 20:15:52 +0000

    datatype

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,12 @@
+let score = "33acs"
+//nodeconsole.log(typeof score);
+// //
+//let valnum = Number(score)
+//console.log(typeof valnum);
+//console.log(valnum); 
+let islogedin = null
+let booleanislogedin = Boolean(islogedin)
+console.log(booleanislogedin);
+let islogedout = 0
+let booleanislogedout = Boolean(islogedout)
+console.log(booleanislogedout);
