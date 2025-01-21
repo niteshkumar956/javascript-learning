78e47b34abb316b2de97ab500d914549f82c1d2d
niteshkumar956 <113203650+niteshkumar956@users.noreply.github.com>
2025-01-21 21:50:44 +0000

    learned

---
+++ 01_basics/05_string.js
@@ -0,0 +1,31 @@
+let name = "nitesh"
+let age = 21
+
+
+
+
+console.log(`Name of the learner of JavaScripts is ${name} an his Age is ${age}`);
+
+
+
+
+const text = new String('Nitesh')
+ /*console.log(text[0]);
+ console.log(text.__proto__);*/
+ console.log(text.toUpperCase());
+ console.log(text.charAt);
+
+ const newstring = text.substring(0,4);
+ console.log(newstring);
+
+ const anothertring = text.slice(-6,3);
+ console.log(anothertring);
+const newstringone = "    nitesh 1 "
+  console.log(newstringone);
+  console.log(newstringone.trim());
+
+  const url = "https://orange-tribble-p4q4jjp9pj429rq4.github.dev/"
+
+console.log(url.replace('orange', 'red' ));
+console.log(url);
+console.log(url.includes('red'));
---
+++ 01_basics/06_num_math.js
@@ -0,0 +1,20 @@
+// property of nums
+
+const score = 400
+console.log(score);
+
+
+ const balance = new Number(100)
+ console.log(balance);
+/*
+ console.log(balance.toString().length);
+ console.log(balance.toFixed(3));
+  console.log(Math.random());
+  console.log(Math.random()*10+1);
+  console.log(Math.floor(Math.random()*10)+1);
+*/
+  const min =10
+  const max =20
+  let num = Math.random();
+  console.log(num)
+  console.log(`math out put of${num} is ${Math.floor(num*(max-min+1))+min}`);
